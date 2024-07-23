c7b96f692f74a529708ea1738239a678c3d3ec47
Boni <boni.hazarika@arthmate.co.in>
2024-07-23 22:39:25 +0530

    second  commit

--- src/components/ApiFetch.jsx
+++ src/components/ApiFetch.jsx
@@ -1,41 +1,57 @@
-// src/components/ApiFetch.js
 import React, { useState, useEffect } from 'react';
 
-const ApiFetch = ({lim}) => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+const ApiFetch = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
 
-  useEffect((lim) => {
-    fetch(`https://jsonplaceholder.typicode.com/posts?_page=1&_limit=${lim}`) // Replace with your API endpoint
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-      });
-  }, []);
+  //API to fetch data
+  const fetchData = async () => {
+    setLoading(true); //set loading to show loading prompt
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
+      if (!response.ok) { //if error
+        throw new Error('Network response was not ok'); 
+      }
+      const newData = await response.json();
+      setData(newData);
+      setLoading(false); 
+    } catch (error) {
+      setError('Fetching error: ', error); //Debugging code for fetch
+      setLoading(false);
+    }
+  };
 
-  if (loading) {
-    return <div>Loading API Data...</div>;
-  }
+  //re-renders when limit changes
+  useEffect(() => {
+    fetchData();
+  }, [limit]);
+
+  //handles infinite scroll 
+  const handleScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500 && !loading) {
+      setLimit((prevLimit) => prevLimit + 10);
+    }
+  };
+
+  //re renders when value of loading changes
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [loading]);
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div><h1>Error: {error.message}</h1></div>;
   }
 
   return (
     <div>
       <h1>Data from API</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      {loading && <div>Loading more data...</div>}
     </div>
   );
 };
